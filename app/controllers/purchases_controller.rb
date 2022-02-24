c63714400ab2848e6874ffa01e7a70a89ffc3aaf
irizaga <gibran.irizaga@gmail.com>
2022-02-24 18:07:52 -0300

    "updated controller"

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,25 +5,27 @@ class PurchasesController < ApplicationController
     end
 
     cart_id = purchase_params[:cart_id]
-
     cart = Cart.find_by(id: cart_id)
 
     unless cart
-      return render json: { errors: [{ message: 'Cart not found!' }] }, status: :unprocessable_entity
+      return render json: { errors: [{ message: 'Cart not found!' }] },
+                    status: :unprocessable_entity
     end
 
     user = CreateUser.call(purchase_params[:user], cart)
 
     unless user.valid?
-      return render json: { errors: user.errors.map(&:full_message).map { |message| { message: message } } }, status: :unprocessable_entity
+      return render json: { errors: user.errors.map(&:full_message).map { |message| { message: message } } },
+                    status: :unprocessable_entity
     end
 
-    order = CreateOrder.call(purchase_params, user)
-
+    order = CreateOrder.call(address_params, user)
     CartItems.call(cart.items, order)
+    order.save
 
     unless order.valid?
-      return render json: { errors: order.errors.map(&:full_message).map { |message| { message: message } } }, status: :unprocessable_entity
+      return render json: { errors: order.errors.map(&:full_message).map { |message| { message: message } } },
+                    status: :unprocessable_entity
     end
 
     return render json: { status: :success, order: { id: order.id } }, status: :ok
